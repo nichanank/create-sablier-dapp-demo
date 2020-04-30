70f9571318f07ebfc91301879dc70b1bd2180958
nichanank <n.kesonpat@gmail.com>
2020-05-01 00:41:25 +0700

    first commit

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,98 +1,202 @@
-import React from "react";
-import logo from "./ethereumLogo.png";
+import React, { useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core"
 import { MAINNET_ID, addresses, abis } from "@sablier-app/contracts";
 import { gql } from "apollo-boost";
+import BigNumber from 'bignumber.js'
 import { ethers } from "ethers";
 import { useQuery } from "@apollo/react-hooks";
+import DateTimePicker from 'react-datetime-picker';
+import UncheckedJsonRpcSigner from './signer'
 import "./App.css";
 
-const GET_STREAMS = gql`
-  {
-    streams(first: 10, orderBy: timestamp, orderDirection: desc) {
-      id
-      cancellation {
-        recipientBalance
-        recipientInterest
-        timestamp
-        txhash
-      }
-      deposit
-      exchangeRateInitial
-      ratePerSecond
-      recipient
-      recipientSharePercentage
-      sender
-      senderSharePercentage
-      startTime
-      stopTime
-      timestamp
-      token {
-        id
-        decimals
-        name
-        symbol
-      }
-      txs {
-        id
-        block
-        event
-        from
-        timestamp
-        to
-      }
-      withdrawals {
-        id
-        amount
-      }
-    }
-  }
-`;
+import { injected } from "./connectors"
+import { useEagerConnect, useInactiveListener } from './hooks'
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = ethers.getDefaultProvider();
-  // Create an instance of ethers.Contract
-  // Read more about ethers.js on https://docs.ethers.io/ethers.js/html/api-contract.html
-  const sablierContract = new ethers.Contract(addresses[MAINNET_ID].sablier, abis.sablier, defaultProvider);
-  // A pre-defined address that owns some cDAI tokens
-  const nextStreamId = await sablierContract.nextStreamId();
-  console.log({ nextStreamId: nextStreamId.toString() });
+BigNumber.config({ EXPONENTIAL_AT: 30 })
+
+const MY_STREAMS = gql`
+query streams($account: String!) {
+  senderStreams:streams(where: {sender: $account}){
+    id
+    recipient
+    deposit
+    startTime
+    stopTime
+  }
+  recipientStreams:streams(where: {recipient: $account}) {
+    id
+    sender
+    deposit
+    startTime
+    stopTime
+  }
+}
+`
+
+function getProviderOrSigner(library, account) {
+  return account ? new UncheckedJsonRpcSigner(library.getSigner(account)) : library
 }
 
 function App() {
-  const { loading, error, data } = useQuery(GET_STREAMS);
 
-  React.useEffect(() => {
+  const [deposit, setDeposit] = useState('')
+  const [recipient, setRecipient] = useState('')
+  const [startTime, setStartTime] = useState(new Date())
+  const [stopTime, setStopTime] = useState(new Date())
+  
+  const context = useWeb3React();
+  const {
+    connector,
+    library,
+    chainId,
+    account,
+    activate,
+  } = context;
+
+  const { loading, error, data } = useQuery(MY_STREAMS, {variables: {account},})
+
+  const [activatingConnector, setActivatingConnector] = useState({});
+  useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined);
+    }
+  }, [activatingConnector, connector]);
+
+  // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
+  const triedEager = useEagerConnect();
+
+  // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
+  useInactiveListener(!triedEager || !!activatingConnector);
+
+  useEffect(() => {
     if (!loading && !error && data && data.streams) {
       console.log({ streams: data.streams });
+    } else {
+
+      console.log(data)
     }
   }, [loading, error, data]);
+
+  function changeStartTime(date) {
+    setStartTime(date)
+  }
+
+  function changeStopTime(date) {
+    setStopTime(date)
+  }
+  
+  function renderSenderStreams() {
+    return data.senderStreams.map((stream) => {
+      return(
+        <React.Fragment key={stream.id}>
+          <p><strong>StreamID: {stream.id}</strong></p>
+          <p>Recipient: {stream.recipient}</p>
+          <p>Deposit: {stream.deposit}</p>
+          <p>Start Time: {stream.startTime}</p>
+          <p>Stop Time: {stream.stopTime}</p>
+          <button class="cancel-button" onClick={async() => {
+            const sablier = new ethers.Contract(addresses[chainId].sablier, abis.sablier, getProviderOrSigner(library, account))
+            const cancelTx = await sablier.cancel(stream.id); 
+            await cancelTx.wait();
+          }}>
+            Cancel Stream
+          </button>
+        </React.Fragment>
+      )
+    })
+  }
+
+  function renderRecipientStreams() {
+    return data.recipientStreams.map((stream) => {
+      return(
+        <React.Fragment key={stream.id}>
+          <p><strong>StreamID: {stream.id}</strong></p>
+          <p>Sender: {stream.sender}</p>
+          <p>Deposit: {stream.deposit}</p>
+          <p>Start Time: {stream.startTime}</p>
+          <p>Stop Time: {stream.stopTime}</p>
+          <button class="withdraw-button" onClick={async() => {
+            const sablier = new ethers.Contract(addresses[chainId].sablier, abis.sablier, getProviderOrSigner(library, account))
+            const withdrawTx = await sablier.withdraw(stream.id); 
+            await withdrawTx.wait();
+          }}>
+            Withdraw from Stream
+          </button>
+        </React.Fragment>
+      )
+    })
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="react-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        <button onClick={() => readOnChainData()} style={{ display: "none" }}>
-          Read On-Chain Data
-        </button>
-        <a
-          className="App-link"
-          href="https://ethereum.org/developers/#getting-started"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginTop: "0px" }}
-        >
-          Learn Ethereum
-        </a>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <a className="App-link" href="https://docs.sablier.finance/" target="_blank" rel="noopener noreferrer">
-          Learn Sablier
-        </a>
+        <h1>Let's Create a Payment Stream!</h1>
+          {context.active ? <p className="wallet-info">Wallet Connected: {account}</p> : 
+          <button onClick={() => {
+            setActivatingConnector(injected)
+            activate(injected) 
+          }}>Connect Wallet</button>
+        }
+        <div class="main-container">
+          <div class="input-container">
+            <input
+              class="input"
+              type="number"
+              min="0"
+              placeholder={'Enter deposit amount'}
+              onChange={e => setDeposit(e.target.value)}
+              onKeyPress={e => {
+                const charCode = e.which ? e.which : e.keyCode
+
+                // Prevent 'minus' character
+                if (charCode === 45) {
+                  e.preventDefault()
+                  e.stopPropagation()
+                }
+              }}
+              value={deposit}
+            />
+          </div>
+          <div class="input-container">
+            <input
+              class="input"
+              type="text"
+              placeholder={'Recipient address'}
+              onChange={e => setRecipient(e.target.value)}
+              value={recipient}
+            />
+            </div>
+            <div class="datetime-input-container"><p>Start Time (Local)</p><DateTimePicker onChange={(date) => changeStartTime(date)} value={startTime} /></div>
+            <div class="datetime-input-container"><p>Stop Time (Local)</p><DateTimePicker onChange={(date) => changeStopTime(date)} value={stopTime} /></div>
+            <div class="button-row">
+              <button class="stream-button" onClick={async() => {
+                const sablier = new ethers.Contract(addresses[chainId].sablier, abis.sablier, getProviderOrSigner(library, account))
+                let convertedStartTime = Math.round(startTime.getTime() / 1000)
+                let convertedStopTime = Math.round(stopTime.getTime() / 1000)
+                let convertedDeposit = new BigNumber(deposit).multipliedBy(10 ** 18).toFixed(0)
+                let remainder = new BigNumber(convertedDeposit) % (convertedStopTime - convertedStartTime)
+                let amountToDeposit = new BigNumber(convertedDeposit).minus(remainder).toString()
+                
+                const token = new ethers.Contract("0xc3dbf84abb494ce5199d5d4d815b10ec29529ff8", abis.erc20, getProviderOrSigner(library, account)); 
+                const approveTx = await token.approve(sablier.address, amountToDeposit); 
+                await approveTx.wait();
+                
+                const createStreamTx = await sablier.createStream(recipient, amountToDeposit, token.address, convertedStartTime, convertedStopTime);
+                await createStreamTx.wait();
+              }}>Create Stream</button>
+            </div>
+            <hr/><hr/>
+            <div class="streams-lists-container">
+              <div class="streams-list">
+                <h3>Streams I created</h3>
+                {data ? renderSenderStreams() : null}
+              </div>
+              <div class="streams-list">
+              <h3>Streams to Me</h3>
+                {data ? renderRecipientStreams() : null}
+              </div>
+            </div>
+          </div>
       </header>
     </div>
   );
--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { ethers } from "ethers";
+import { Web3ReactProvider } from '@web3-react/core';
 import "./index.css";
 import App from "./App";
+
+function getLibrary(provider) {
+  const library = new ethers.providers.Web3Provider(provider)
+  library.pollingInterval = 10000
+  return library
+}
 
 // This is the official Sablier subgraph. You can replace it with your own, if you need to.
 // See all subgraphs: https://thegraph.com/explorer/
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/sablierhq/sablier",
+  uri: "https://api.thegraph.com/subgraphs/name/sablierhq/sablier-rinkeby",
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Web3ReactProvider getLibrary={getLibrary}>
+          <ApolloProvider client={client}>
+            <App />
+          </ApolloProvider>
+  </Web3ReactProvider>,
   document.getElementById("root"),
 );
